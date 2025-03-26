dc02d086dcd6a260d055fd9a99cef4ac31481159
jpancorb <juanmipancorbo@gmail.com>
2025-03-26 19:22:47 +0100

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,9 +6,24 @@
 /*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 20:20:21 by jpancorb          #+#    #+#             */
-/*   Updated: 2025/03/25 20:21:58 by jpancorb         ###   ########.fr       */
+/*   Updated: 2025/03/26 19:20:45 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+Zombie*	newZombie(std::string name);
+void	randomChump(std::string name);
+
+int	main()
+{
+	std::cout << "Creating a zombie with newZombie (HEAP allocation):" << std::endl;
+	Zombie* heapZombie = newZombie("BARTOLO");
+	heapZombie->announce();
+	delete heapZombie;	// Frees the memory allocated on the HEAP
+
+	std::cout << "\nCreating a zombie with randomChump (STACK allocation):" << std::endl;
+	randomChump("JUANCAR");
+
+	return 0;
+}
