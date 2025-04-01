050595749df40cce1debaede61e3790cebf2dadb
jpancorb <juanmipancorbo@gmail.com>
2025-04-01 19:52:37 +0200

    ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpancorb <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 19:30:26 by jpancorb          #+#    #+#             */
+/*   Updated: 2025/04/01 19:48:11 by jpancorb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	// variables
+	std::string brain		= "HI THIS IS BRAIN";
+	std::string *stringPTR	= &brain;
+	std::string &stringREF	= brain;
+
+	// to print addresses
+	std::cout << "Memory address of string variable: " << &brain << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+
+	// to print values
+	std::cout << "Value of string variable: " << brain << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
